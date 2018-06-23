20400a0f8b52844ccbaf534c619bf34c1b6e55fb
itisha23 <itishagupta.2396@gmail.com>
2018-06-23 05:34:24 +0530

    Update uglyNumbers.cpp

--- uglyNumbers.cpp
+++ uglyNumbers.cpp
@@ -14,16 +14,7 @@ typedef unsigned int ul;
 typedef long long int ll;
 typedef unsigned long long int ull;
 typedef vector<ll> vi;
-ll arr[501];
-
-void increase(ll &ptr,ll a,set<ll>se)
-{
-    ptr++;
-    while(se.find(arr[ptr]*a)!=se.end())
-    {
-        ptr++;
-    }
-}  
+ll arr[501]; 
   
 
 int main()
@@ -36,31 +27,26 @@ int main()
       ll n,count,p2,p3,p5,mini,a,b,c,i;
       cin>>n;
       set<ll>se;
-      arr[1]=1;arr[2]=2;arr[3]=3;arr[4]=4;arr[5]=5;arr[6]=6;arr[7]=8;arr[8]=9;arr[9]=10;arr[10]=12;
-      se.insert(1);  se.insert(2);  se.insert(3);  se.insert(4);  se.insert(5);
-      if(n<=10)
-      cout<<arr[n]<<endl;
-      else
+      arr[0]=1;
+      arr[1]=1;
+      count=0;
+      p2=1;p3=1;p5=1;
+      while(count!=n)
       {
-          count=10;
-          p2=7;p3=5;p5=3;
-          while(count!=n)
-          {
-              a=arr[p2]*2;
-              b=arr[p3]*3;
-              c=arr[p5]*5;
-              
-              mini=min(a,min(b,c));
-              se.insert(mini);
-              arr[++count]=mini;
-          
-              if(mini==a)
-              increase(p2,2,se);
-              if(mini==b)
-              increase(p3,3,se);
-              if(mini==c)
-              increase(p5,5,se);
-          }
+          a=arr[p2]*2;
+          b=arr[p3]*3;
+          c=arr[p5]*5;
+
+          mini=min(a,min(b,c));
+          arr[++count]=mini;
+
+          if(mini==a)
+          p2++;
+          if(mini==b)
+          p3++;
+          if(mini==c)
+          p5++;
+       }
          cout<<arr[n]<<endl;
       }
       
