6568f253156bf44b294724461aa25afa3ee9e3ce
itisha23 <itishagupta.2396@gmail.com>
2018-07-01 23:47:05 +0530

    Update fraction.cpp

--- fraction.cpp
+++ fraction.cpp
@@ -27,25 +27,26 @@ string Solution::fractionToDecimal(int num, int dum) {
     map<long long int,long long int>mp;
     long long int  rem,a,q,p,flag=0,i;
     string ans="",h;
-    string sn=convert(n);
-    rem=0;
-    for(i=0;i<sn.size();i++)
+    //string sn=convert(n);
+    //rem=0;
+    /*for(i=0;i<sn.size();i++)
     {
         a=(rem*10)+sn[i]-'0';
         rem=a%d;
         q=a/d;
         ans=ans+convert(q);
         
-    }
-  while(ans[0]=='0')
-    ans.erase(0,1);
-         if(ans.size()==0)
-          ans=ans+"0";
+    }*/
+    ans=ans+convert(n/d);
+    rem=n%d;
+  //while(ans[0]=='0')
+    //ans.erase(0,1);
+       //  if(ans.size()==0)
+         // ans=ans+"0";
           
     if(rem!=0)
-    {
-        ans=ans+".";
-    }
+    ans=ans+".";
+
     p=ans.size();
     while(rem!=0 && flag==0)
     {
@@ -54,23 +55,15 @@ string Solution::fractionToDecimal(int num, int dum) {
         q=a/d;
         if(mp.count(rem)==0)
         {
-            mp[rem]=q;
+           
            ans=ans+convert(q);
+            mp[rem]=ans.size();
            rem=a%d;
         }
         else
         {
-            int ind;
-            for(i=p;i<ans.size();i++)
-            {
-                if(mp[rem]==ans[i]-'0')
-                   {
-                       ind=i;
-                       break;
-                   }
-            }
-            //cout<<ind<<endl;
-            ans=ans.substr(0,ind)+"("+ans.substr(ind,ans.size()-ind)+")";
+            int ind=mp[rem];
+            ans=ans.substr(0,ind-1)+"("+ans.substr(ind-1,ans.size()-ind+1)+")";
           flag=1;
         }
     }
