8f6e4051f6ab4228236d2c73e9ee13074dc297d0
itisha23 <itishagupta.2396@gmail.com>
2018-07-01 10:30:37 +0530

    Update dutchNationalFlagAlgorithm.cpp

--- dutchNationalFlagAlgorithm.cpp
+++ dutchNationalFlagAlgorithm.cpp
@@ -15,39 +15,42 @@ typedef long long int ll;
 typedef unsigned long long int ull;
 typedef vector<ll> vi;
 
-void sort012(ll arr[], ll  arr_size)
-{
-    int lo = 0;
-    int hi = arr_size - 1;
-    int mid = 0;
- 
-    while (mid <= hi)
-    {
-        switch (arr[mid])
-        {
-        case 0:
-            swap(arr[lo++], arr[mid++]);
-            break;
-        case 1:
-            mid++;
-            break;
-        case 2:
-            swap(arr[mid],arr[hi--]);
-            break;
-        }
-    }
+void sort012 (vector<int> &vec) {
+     int n=vec.size(),i=0,j=0,k=n-1;
+     while(j<n && k>=j)
+     {
+         if(vec[j]==1)
+         {
+             j++;
+             continue;
+         }
+         else if(vec[j]==0)
+         {
+             swap(vec[i],vec[j]);
+             i++;
+             j++;
+         }
+         else
+         {
+             swap(vec[j],vec[k]);
+             k--;
+         }
+     }
+     
+
 }
 
 int main()
 {
     ll n,i;
     cin>>n;
+    vector<int>vec(n);
     ll arr[n];
     f(i,0,n)
-      cin>>arr[i];
-    sort012(arr, n);
+      cin>>vec[i];
+    sort012(vec, n);
     f(i,0,n)
-      cout<<arr[i]<<"  ";
+      cout<<vec[i]<<"  ";
     cout<<endl;
 
     
