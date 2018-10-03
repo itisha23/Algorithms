97dc2a79309daf0b84fc5598309a96f5a6e7a9f5
itisha23 <itishagupta.2396@gmail.com>
2018-10-03 13:01:09 +0530

    Create removeDuplicatesFromASortedArray.cpp

---
+++ removeDuplicatesFromASortedArray.cpp
@@ -0,0 +1,22 @@
+int remove_duplicate(int arr[],int n)
+{
+   if(n==0 || n==1)
+   return n;
+   
+   int i,j;
+   j=0;
+   for(i=0;i<n-1;i++)
+   {
+       if(arr[i]!=arr[i+1])
+       {
+           arr[j]=arr[i];
+           j++;
+       }a
+   }
+   arr[j]=arr[n-1];
+   /*for(i=0;i<=j;i++)
+   cout<<arr[i]<<" ";
+   cout<<endl;*/
+   
+   return j+1;
+}
