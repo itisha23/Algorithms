61774f0d9a1e63316e56a7227ff2f9c817dcc3a5
itisha23 <itishagupta.2396@gmail.com>
2019-07-03 05:15:02 +0530

    Create lowestCommonAncestorInBinaryTree.cpp

---
+++ Tree/lowestCommonAncestorInBinaryTree.cpp
@@ -0,0 +1,31 @@
+/*The method 1 finds LCA in O(n) time, but requires three tree traversals plus extra spaces for path arrays. If we assume that the keys n1
+and n2 are present in Binary Tree, we can find LCA using single traversal of Binary Tree and without extra storage for path arrays.
+The idea is to traverse the tree starting from root. If any of the given keys (n1 and n2) matches with root, then root is LCA (assuming 
+that both keys are present). If root doesn’t match with any of the keys, we recur for left and right subtree. The node which has one key 
+present in its left subtree and the other key present 
+in right subtree is the LCA. If both keys lie in left subtree, then left subtree has LCA also, otherwise LCA lies in right subtree.*/
+
+
+struct Node *findLCA(struct Node* root, int n1, int n2) 
+{ 
+    // Base case 
+    if (root == NULL) return NULL; 
+  
+    // If either n1 or n2 matches with root's key, report 
+    // the presence by returning root (Note that if a key is 
+    // ancestor of other, then the ancestor key becomes LCA 
+    if (root->key == n1 || root->key == n2) 
+        return root; 
+  
+    // Look for keys in left and right subtrees 
+    Node *left_lca  = findLCA(root->left, n1, n2); 
+    Node *right_lca = findLCA(root->right, n1, n2); 
+  
+    // If both of the above calls return Non-NULL, then one key 
+    // is present in once subtree and other is present in other, 
+    // So this node is the LCA 
+    if (left_lca && right_lca)  return root; 
+  
+    // Otherwise check if left subtree or right subtree is LCA 
+    return (left_lca != NULL)? left_lca: right_lca; 
+} 
