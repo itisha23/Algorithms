7d531ffce0dee6206b43cfaaf8a186690304240a
itisha <itisha.gupta@facstet.com>
2020-02-12 06:48:12 +0530

    binary tree pruning

---
+++ Tree/binaryTreePruning.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    TreeNode* pruneTree(TreeNode* root) {
+        if(root == NULL)
+            return  NULL;
+        
+       root->left = pruneTree(root->left);
+       root->right = pruneTree(root->right);
+        
+        if(root->val == 0 && root->left== NULL && root->right == NULL ){
+            delete(root);
+            return NULL;
+        }
+        return root;
+    }
+};
