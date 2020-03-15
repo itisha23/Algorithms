fef396b89016d2c17668c1d412bd3b76f8b0d7df
itisha <itisha.gupta@facstet.com>
2020-03-15 11:14:13 +0530

    modify n queens

--- Backtracking/NQueens.cpp
+++ Backtracking/NQueens.cpp
@@ -1,61 +1,62 @@
-bool isSafe(int k,int i,int x[])
-{
-    int j;
-    for(j=1;j<k;j++)
-    {
-        if(x[j]==i || abs(j-k)==abs(x[j]-i))
-         return false;
+class Solution {
+public:
+    bool isSafe(int k, int j, vector<string>v, int n) {
     
+    int i;
+    // same column
+    for(i=0; i< k;i++) {
+        if(v[i][j]!='.'){
+            return false;
+        }
     }
+    
+    // same diagonal -
+    for(i=0; i<k ;i++) {
+        if(j-(k-i) >=0 && v[i][j-(k-i)] != '.'){
+            return false;
+        }
+    }
+    
+    //same diagonal +
+    for(i=0; i<k ;i++) {
+        if(j+(k-i) < n && v[i][j+(k-i)] != '.'){
+            return false;
+        }
+    }
+    
     return true;
 }
-
-void func(vector<vector<string> >&vec, vector<string>&v,int k,int n,int x[])
-{
-    int i,p;
-    string s;
-    for(i=1;i<=n;i++)
-    {
-        if(isSafe(k,i,x))
-        {
-            x[k]=i;
-            s="";
-            for(p=1;p<=n;p++)
-            {
-               if(p==i)
-                s=s+"Q";
-               else
-                s=s+".";
-            }
-            v.push_back(s);
-            if(k==n)
-            {
-                vec.push_back(v);
-               
-                
-            }
-            else
-            {
-                func(vec,v,k+1,n,x);
-                 
-            }
-            v.pop_back();
+void func(vector<vector<string>>&ans, vector<string>&v, int k, int n) {
+    
+    if(k == n){
+        ans.push_back(v);
+        return;
+    }
+    int j;
+    
+    for(j=0; j<n ;j++) {
+        if(isSafe(k, j, v, n )) {
+            v[k][j] = 'Q';
+            func(ans, v, k+1, n);
+            v[k][j] = '.';
         }
     }
 }
-vector<vector<string> > Solution::solveNQueens(int n) {
-    // Do not write main() function.
-    // Do not read input, instead use the arguments to the function.
-    // Do not print the output, instead return values as specified
-    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
-    int i,j;
-    vector<vector<string> >vec;
-    vector<string>v;
-    int x[n+1];
-    func(vec,v,1,n,x);
-    reverse(vec.begin(),vec.end());
-    return vec;
-}
-
-
+    vector<vector<string>> solveNQueens(int n) {
+        
+        vector<vector<string>>ans;
+        vector<string>v;
+        
+        int k=0, i;
+        string s="";
+        for(i=0; i<n;i++){
+            s = s+'.';
+        }
+        for(i=0; i<n; i++){
+            v.push_back(s);
+        }
+        func(ans, v,k, n);
+        return ans;
+    }
+};
 
